d952dc532be2bbce70323830bc3b72a5a607a592
Victor Alando <124558542+WebXpertAlando@users.noreply.github.com>
2025-02-20 14:16:44 +0300

    Update Home.py

--- Home.py
+++ Home.py
@@ -15,18 +15,18 @@ def get_db_connection():
 
 # Function to create users table
     def create_users_table():
-    conn = get_db_connection()
-    cur = conn.cursor()
-    cur.execute("""
+        conn = get_db_connection()
+        cur = conn.cursor()
+        cur.execute("""
         CREATE TABLE IF NOT EXISTS users (
             id SERIAL PRIMARY KEY,
             username VARCHAR(50) UNIQUE NOT NULL,
             password TEXT NOT NULL
-        )
-    """)
-    conn.commit()
-    cur.close()
-    conn.close()
+            )
+            """)
+         conn.commit()
+        cur.close()
+        conn.close()
 
 
 
