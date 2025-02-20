5f3d754b4be95abdef6a7832da966c30c2769d19
Victor Alando <124558542+WebXpertAlando@users.noreply.github.com>
2025-02-20 16:05:25 +0300

    Update Learn Retention Strategy.py

--- pages/Learn Retention Strategy.py
+++ pages/Learn Retention Strategy.py
@@ -1,1 +1,21 @@
+import streamlit as st
+from PIL import Image
 
+# Load an image
+image = Image.open('images/customer-retention.jpg')
+
+# Display the image
+st.image(image, use_container_width=True)
+
+st.markdown(""'
+A customer retention strategy helps you keep your customers around longer, which helps you avoid having to go out and find new ones.
+
+- Get to the root of the Churn.
+- Customer Segmentation.
+- Monitor User Engagement.
+- Create an intuitive user onboarding process.
+- Offer personalized and proactive customer communication.
+- Reward customer loyalty .
+- Follow up with customers.
+- Develop a data roadmap and stick to it.
+""")
