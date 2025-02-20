78e80e930fe37c3aec11747736cda047dac50ffc
Victor Alando <124558542+WebXpertAlando@users.noreply.github.com>
2025-02-20 16:18:26 +0300

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,1 +1,17 @@
+import streamlit as st
+
+def footer():
+    st.markdown("---")
+    st.markdown(
+        "© 2025 DataAnalyticsXpert | Developed by [Vic Alando](https://github.com/WebXpertAlandob) "
+        " | Contact: 0723 205 119 | Email: [vicadventist@gmail.com](mailto:your@email.com)",
+        unsafe_allow_html=True,
+    )
+
+# Example Streamlit app
+st.title("My Streamlit App")
+st.write("Main content goes here.")
+
+# Add footer
+footer()
 
