f50035324ceb473dd3aac56e82767d4a894bebcf
Victor Alando <124558542+WebXpertAlando@users.noreply.github.com>
2025-02-20 16:08:51 +0300

    Update Learn Retention Strategy.py

--- pages/Learn Retention Strategy.py
+++ pages/Learn Retention Strategy.py
@@ -2,7 +2,7 @@ import streamlit as st
 from PIL import Image
 
 # Load an image
-image = Image.open('images/customer-retention.jpg')
+image = Image.open('images/custome-retention.jpg')
 
 # Display the image
 st.image(image, use_container_width=True)
