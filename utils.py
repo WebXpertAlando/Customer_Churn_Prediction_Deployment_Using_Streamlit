359e13825b4d7d646491f691c0c30469e9aae62e
Victor Alando <124558542+WebXpertAlando@users.noreply.github.com>
2025-02-20 16:23:06 +0300

    Update utils.py

--- utils.py
+++ utils.py
@@ -3,7 +3,7 @@ import streamlit as st
 def footer():
     st.markdown("---")
     st.markdown(
-        "© 2025 DataAnalyticsXpert | Developed by [Vic Alando](https://github.com/WebXpertAlando) "
+        "© 2025  Developed by [Vic Alando](https://github.com/WebXpertAlando) "
         " | Contact: 0723 205 119 | Email: [vicadventist@gmail.com](mailto:vicadventist@gmail.com)",
         unsafe_allow_html=True,
     )
