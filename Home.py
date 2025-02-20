e9b618bc00e7ffd2f89e71161495f2a83bf963df
Victor Alando <124558542+WebXpertAlando@users.noreply.github.com>
2025-02-20 15:02:13 +0300

    Update Home.py

--- Home.py
+++ Home.py
@@ -2,7 +2,7 @@ import streamlit as st
 from PIL import Image
 
 # Load an image
-image = Image.open('customer_churn.jpg')
+image = Image.open('images/customer_churn.jpg')
 
 # Display the image
 st.image(image, caption="Churn", use_column_width=True)
