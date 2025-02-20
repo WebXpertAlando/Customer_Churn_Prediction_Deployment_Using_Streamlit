a5389ee8848360a094c195e6c94ac82c37974187
Victor Alando <124558542+WebXpertAlando@users.noreply.github.com>
2025-02-20 15:20:40 +0300

    Update Home.py

--- Home.py
+++ Home.py
@@ -9,3 +9,12 @@ image = Image.open('images/customer-churn.jpg')
 
 # Display the image
 st.image(image, use_container_width=True)
+
+<h1 style="color:#000">Empowering Users, Democratizing Data</h1>
+st.write("Our app goes beyond predictive analytics, democratizing data and putting the power in your hands")
+st.write("Make data-driven decisions, reduce churn rates, and drive business success").
+
+ <div style="background-color:#ffd700;padding:10px;border-radius:10px">
+    <h1 style="color:#000">GET STARTED TODAY!</h1>
+    <p style="color:#000">Explore this app and discover the value of accurate churn prediction.</p>
+    </div>
